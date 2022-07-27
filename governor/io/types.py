a8af06d9f7aaddd6e6b059b5057a250f4d353ca5
mproissl <mproissl@gmail.com>
2022-07-27 19:28:41 +0200

    Add config_header_parameters and config_payload_operator_parameters

--- governor/io/types.py
+++ governor/io/types.py
@@ -40,3 +40,41 @@ def config_type_description():
         ConfigType.JSON: "JSON File Configuration.",
         ConfigType.JSON_STRING: "JSON-String Configuration."
     }
+
+
+def config_header_parameters():
+    """Configuration header parameters with descriptions."""
+    return {
+        "name": "Name of the configuration, e.g. with reference to its use.",
+        
+        "description": "Longer description of the configuration or use case purpose.",
+        
+        "enable_multiprocessing": "Flag to create multiple processes to run operators in parallel. Default: True",
+        
+
+    }
+
+
+def config_payload_operator_parameters():
+    """Configuration operator parameters with descriptions."""
+    return {
+        "id_": "Globally unique identifier of operator. Default: automatically generated.",
+        
+        "name": "Name of operator and thus Python method name to run.",
+        
+        "label": "Custom label attached to this operator, which may describe the operator better than the name.",
+        
+        "module_path": "Python path to operator module using dots convention.",
+        
+        "class_name": "Name of the class containing the operator method to run.",
+        
+        "class_params": "Dictionary of parameter settings to pass to class containing the operator method using the keys for parameter names and respective values for parameter values.",
+        
+        "dedicated_input_params": "Dictionary of dedicated parameter settings to pass to the operator method using the keys for parameter names and respective values for parameter values.",
+        
+        "shared_input_params": "Either the name of an existing globally-shared object or new object of the tuple shape ('OBJECT_NAME', initial-value).",
+        
+        "save_output": "If the operator has a return value, this flag can be set to save the response. Default: False",
+        
+        "shared_output_name": "If save_output is set to True, the response can be shared with other operators given this globally-unique user-defined name."
+    }
