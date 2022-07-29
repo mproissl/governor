935eb6feffd6190c1e02d77bedc1263d447ac519
mproissl <mproissl@gmail.com>
2022-07-29 13:46:00 +0200

    Update config types

--- governor/io/types.py
+++ governor/io/types.py
@@ -31,6 +31,7 @@ class ConfigType(_Enum):
     YAML = 0
     JSON = 1
     JSON_STRING = 2
+    DICT = 3
 
 
 def config_type_description():
@@ -38,7 +39,8 @@ def config_type_description():
     return {
         ConfigType.YAML: "YAML File Configuration.",
         ConfigType.JSON: "JSON File Configuration.",
-        ConfigType.JSON_STRING: "JSON-String Configuration."
+        ConfigType.JSON_STRING: "JSON-String Configuration.",
+        ConfigType.DICT: "Dictionary Configuration.",
     }
 
 
@@ -46,11 +48,12 @@ def config_header_parameters():
     """Configuration header parameters with descriptions."""
     return {
         "name": "Name of the configuration, e.g. with reference to its use.",
-        
-        "description": "Longer description of the configuration or use case purpose.",
-        
-        "enable_multiprocessing": "Flag to create multiple processes to run operators in parallel. Default: True",
-        
+
+        "description": "Longer description of the configuration or use case "\
+                       "purpose.",
+
+        "enable_multiprocessing": "Flag to create multiple processes to run "\
+                                  "operators in parallel. Default: True",
 
     }
 
@@ -58,23 +61,123 @@ def config_header_parameters():
 def config_payload_operator_parameters():
     """Configuration operator parameters with descriptions."""
     return {
-        "id_": "Globally unique identifier of operator. Default: automatically generated.",
-        
-        "name": "Name of operator and thus Python method name to run.",
-        
-        "label": "Custom label attached to this operator, which may describe the operator better than the name.",
-        
-        "module_path": "Python path to operator module using dots convention.",
-        
-        "class_name": "Name of the class containing the operator method to run.",
-        
-        "class_params": "Dictionary of parameter settings to pass to class containing the operator method using the keys for parameter names and respective values for parameter values.",
-        
-        "dedicated_input_params": "Dictionary of dedicated parameter settings to pass to the operator method using the keys for parameter names and respective values for parameter values.",
-        
-        "shared_input_params": "Either the name of an existing globally-shared object or new object of the tuple shape ('OBJECT_NAME', initial-value).",
-        
-        "save_output": "If the operator has a return value, this flag can be set to save the response. Default: False",
-        
-        "shared_output_name": "If save_output is set to True, the response can be shared with other operators given this globally-unique user-defined name."
+        "id_":
+            "Globally unique identifier of operator. Default: "\
+            "automatically generated.",
+
+        "name":
+            "Name of operator and thus Python method name to run.",
+
+        "label":
+            "Custom label attached to this operator, which may describe "\
+            "the operator better than the name.",
+
+        "module_path":
+            "Python path to operator module using dots convention.",
+
+        "class_name":
+            "Name of the class containing the operator method to run.",
+
+        "class_params":
+            "Dictionary of parameter settings to pass to class "\
+            "containing the operator method using the keys for "\
+            "parameter names and respective values for parameter "\
+            "values.",
+
+        "dedicated_input_params":
+            "Dictionary of dedicated parameter "\
+            "settings to pass to the operator method "\
+            "using the keys for parameter names and "\
+            "respective values for parameter values.",
+
+        "shared_input_params":
+            "Either the name of an existing globally-"\
+            "shared object or new object of the tuple "\
+            "shape ('OBJECT_NAME', initial-value).",
+
+        "save_output":
+            "If the operator has a return value, this flag can "\
+            "be set to save the response. Default: False",
+
+        "shared_output_name":
+            "If save_output is set to True, the response "\
+            "can be shared with other operators given "\
+            "this globally-unique user-defined name.",
+
+        "run_after":
+            "Command to run this operator after completion of other "\
+            "operator(s) given one (String) or more (list) of id_ "\
+            "values assigned to the respective operators. If this "\
+            "parameter is not set, operators are executed in the "\
+            "order they appear in the configuration.",
+
+        "run_after_parallel":
+            "Boolean flag to allow parallel execution of "\
+            "this operator if more than one run_after "\
+            "id_ has been set. Default: False",
+
+        "group":
+            "User-defined group name which adds this operator to a "\
+            "group of other operators, which belong together and "\
+            "can be referenced as one larger operator.",
+
+        "repeat":
+            "Number of times to run this operator. Default: 1",
+
+        "dedicated_input_variations":
+            "Dictionary of input parameter variations with the name of the "\
+            "parameter as key and a dictionary as value with either 'from', "\
+            "'to', 'step_size' for numerical settings or 'list' for a list "\
+            "of any values.",
+
+        "run_synchronized_input_variations:":
+            "Boolean flag to indicate if parameter values specified in "\
+            "'dedicated_input_variations' shall be varied all at once, "\
+            "which requires that the length must be equal over all variation "\
+            "instructions, or one parameter is varied at a time and thus all "\
+            "permutations are being run. Default: False",
+
+        "run_group_level_variations":
+            "Boolean flag to run variations specified in "\
+            "'dedicated_input_variations' after running all other operators "\
+            "in the 'group'. Default: False"
+
+    }
+
+
+def config_payload_variation_parameters():
+    """Configuration variation parameters with descriptions."""
+    return {
+        "repeat_groups":
+            "Number of times to run a group of operators. Default: 1",
+
+        "group_input_variations":
+            "Dictionary of input parameter variations to apply to operators "\
+            "belonging to the respective group using the following structure: "\
+            "GROUP_NAME: PARAM_NAME: (dict with 'from', 'to', 'step_size' "\
+            "for numerical settings or 'list' for a list of any values).",
+
+        "run_synchronized_input_variations:":
+            "Boolean flag to indicate if parameter values specified in "\
+            "'group_input_variations' shall be varied all at once, "\
+            "which requires that the length must be equal over all variation "\
+            "instructions, or one parameter is varied at a time and thus all "\
+            "permutations are being run. Default: False"
+    }
+
+
+def config_payload_variation_options():
+    """Configuration options for variation parameters."""
+    return {
+        "range_based":
+            {
+                "from": float,
+                "to": float,
+                "step_size": float
+            },
+
+        "list_based":
+            {
+                "list": list
+            }
     }
