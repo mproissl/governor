eda70b507a4936edfc749e070c42d9a22224ef02
mproissl <mproissl@gmail.com>
2022-08-08 18:41:00 +0200

    Add more simple test operators

--- tests/test_modules/operators/native_based.py
+++ tests/test_modules/operators/native_based.py
@@ -15,24 +15,40 @@ def operator_no_shared_no_return(value_str: str, value_int: int):
 
 
 def operator_no_return(value_str: str,
-                       governor_shared: dict,
-                       me_: str = _inspect.currentframe().f_code.co_name):
+                       shared_param_1: str,
+                       me_: str = None):
     """Operator with parameters, shared and no return"""
-
-    # Modify
-    if len(governor_shared) > 0:
-        governor_shared[next(iter(governor_shared))] = value_str
-
+    if me_ is None:
+        me_ = _inspect.currentframe().f_code.co_name
     print(me_,\
           ": str=", value_str,\
-          "shared=", governor_shared)
+          "shared_param_1=", shared_param_1)
 
 
 def operator(value_str: str,
-             governor_shared: dict):
+             shared_param_2: str) -> bool:
     """Operator with parameters, shared and return"""
 
     operator_no_return(value_str,
-                       governor_shared,
+                       shared_param_2,
                        me_=_inspect.currentframe().f_code.co_name)
     return True
+
+
+def operator_multi_share(value_str: str,
+                         shared_param_1: str,
+                         shared_param_2: str):
+    """Operator with parameters, multi-shared and no return"""
+    print(_inspect.currentframe().f_code.co_name,\
+          ": str=", value_str,\
+          "shared_param_1=", shared_param_1,\
+          "shared_param_2=", shared_param_2)
+
+
+def operator_add_to_shared(value_to_add: int,
+                           shared_param_3: int):
+    """Operator with parameters with returned addition to shared parameter."""
+    print(_inspect.currentframe().f_code.co_name,\
+          ": value_to_add=", value_to_add,\
+          "shared_param_3=", shared_param_3)
+    return shared_param_3 + value_to_add
