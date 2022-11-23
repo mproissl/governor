3a47fe2c7333940646504fbe3fb6e59c0d5a6f40
mproissl <mproissl@gmail.com>
2022-11-23 13:35:58 +0100

    Update multiproc tests to more complex cases

--- tests/test_modules/operators/native_multiproc_based.py
+++ tests/test_modules/operators/native_multiproc_based.py
@@ -5,12 +5,31 @@ from time import sleep
 from datetime import datetime
 
 
-def operator_task(iterations: int, process_time_sec: int):
+def job_noreturn(id_: str, iterations: int, process_time_sec: int):
     """Task with no shared and no return"""
-    for i in range(0, iterations):
+    print(datetime.now().strftime("%H:%M:%S"),
+          "START",
+          id_,
+          _inspect.currentframe().f_code.co_name,
+          "N="+str(iterations),
+          "T="+process_time_sec+"s"
+         )
+    for _ in range(iterations):
         sleep(process_time_sec)
-        print(_inspect.currentframe().f_code.co_name, i, datetime.now())
+    print(datetime.now().strftime("%H:%M:%S"),
+          "END",
+          id_)
 
-def operator_post(msg: str):
-    """Post-Task with no shared and no return"""
-    print(_inspect.currentframe().f_code.co_name, msg)
+def job_return(id_: str, process_time_sec: int, msg: str):
+    """Task with no shared and return"""
+    print(datetime.now().strftime("%H:%M:%S"),
+          "START",
+          id_,
+          _inspect.currentframe().f_code.co_name,
+          "T="+process_time_sec+"s"
+         )
+    sleep(process_time_sec)
+    print(datetime.now().strftime("%H:%M:%S"),
+          "END",
+          id_)
+    return msg
