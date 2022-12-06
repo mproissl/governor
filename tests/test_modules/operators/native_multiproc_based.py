93b558d09a0025cfd48ecef5cfa131ba9a9a463d
mproissl <mproissl@gmail.com>
2022-12-06 12:33:41 +0100

    Update print-out statements

--- tests/test_modules/operators/native_multiproc_based.py
+++ tests/test_modules/operators/native_multiproc_based.py
@@ -7,29 +7,33 @@ from datetime import datetime
 
 def job_noreturn(id_: str, iterations: int, process_time_sec: int):
     """Task with no shared and no return"""
-    print(datetime.now().strftime("%H:%M:%S"),
-          "START",
-          id_,
-          _inspect.currentframe().f_code.co_name,
-          "N="+str(iterations),
-          "T="+process_time_sec+"s"
+    print(datetime.now().strftime("%H:%M:%S")+
+          " START "+
+          id_+ ": "+
+          _inspect.currentframe().f_code.co_name+
+          " N="+str(iterations)+
+          " T="+str(process_time_sec)+"s",
+          flush=True
          )
     for _ in range(iterations):
         sleep(process_time_sec)
-    print(datetime.now().strftime("%H:%M:%S"),
-          "END",
-          id_)
+    print(datetime.now().strftime("%H:%M:%S")+
+          " END "+
+          id_,
+          flush=True)
 
 def job_return(id_: str, process_time_sec: int, msg: str):
     """Task with no shared and return"""
-    print(datetime.now().strftime("%H:%M:%S"),
-          "START",
-          id_,
-          _inspect.currentframe().f_code.co_name,
-          "T="+process_time_sec+"s"
+    print(datetime.now().strftime("%H:%M:%S")+
+          " START "+
+          id_+ ": "+
+          _inspect.currentframe().f_code.co_name+
+          " T="+str(process_time_sec)+"s",
+          flush=True
          )
     sleep(process_time_sec)
-    print(datetime.now().strftime("%H:%M:%S"),
-          "END",
-          id_)
+    print(datetime.now().strftime("%H:%M:%S")+
+          " END "+
+          id_,
+          flush=True)
     return msg
